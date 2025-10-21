89ae77ec2c9e4ffa97d5997a50338ee9f2aa5b98
LAPTOP-2R9HJ3QS\tangx <Xuening.tang@student.uva.nl>
2025-10-21 17:51:15 +0200

    Implement master agent, but need further revision on role differentiation and feedback loop

--- code generator/coding_agent.py
+++ code generator/coding_agent.py
@@ -2,6 +2,7 @@
 import os
 from openai import OpenAI
 import json
+import re
 
 class CodingAgent:
     def __init__(self, model_name="gpt-4o-mini"):
@@ -86,6 +87,7 @@ class SubCodeAgents:
         The shared_instructions also specify which package you can import.
         Do not invent any new names or parameters, to ensure the compatibility with other code files.
         The code file should be clean, well-commented, modular and runnable.
+        Do NOT provide any explanations or notes outside the code. Just provide the code for the specified module.
         """
 
         user_prompt = f"""
@@ -95,7 +97,7 @@ class SubCodeAgents:
         """
 
         # call the LLM
-        LLM_model = self.client.chat.completuons.create(
+        LLM_model = self.client.chat.completions.create(
             model = self.model_name,
             messages = [
                 {"role": "system", "content": system_prompt},
@@ -112,3 +114,171 @@ class SubCodeAgents:
     
     #TODO: implement the master agent
 
+class MasterAgent:
+    def __init__(self, conceptual_model, model_name="gpt-4o-mini"):
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))  # set your API key in environment variable
+        self.model_name = model_name  # default model (maybe a stronger model is needed here)
+        self.conceptual_model = conceptual_model # input json path here
+        self.sub_agents = {
+            "agent.py": SubCodeAgents("agent.py", model_name),
+            "environment.py": SubCodeAgents("environment.py", model_name),
+            "model.py": SubCodeAgents("model.py", model_name),
+            "run.py": SubCodeAgents("run.py", model_name),
+            "visualization.py": SubCodeAgents("visualization.py", model_name)
+        }  # sub-coding agents of the master agent
+    """
+    The master agent supervises and coordinate the whole code generation process.
+    Its task involves:
+    (1) Read the input conceptual model and decompose it into sub-components
+    (agent.py, environment.py, model.py, run.py, visualization.py).
+    (2) Create a list of shared instructions for all sub-coding agents, specifying coding conventions,
+    dependencies, and integration points to ensure compatibility across different code files.
+    (3) Assign each sum-component and the list of shared insturctions to the sub-coding agents.
+    (4) Collect the generated code snippets from each sub-coding agent and integrate them into a cohesive codebase.
+    (5) Perform a final review and run the integrated code to ensure functionality and correctness.
+    (6) If issues arise, provide feedback to the relevant sub-coding agent for revisions
+    (7) Output the final complete codebase if all components function correctly.
+    """
+
+    def shared_instructions_generator(self):
+        """
+        This function generates a list of shared instructions for all sub-coding agents
+        based on the overall conceptual model.
+        """
+        # load the conceptual model
+        with open(self.conceptual_model, "r") as f:
+            conceptual_model = json.load(f)
+
+        # prompting the LLM (revise later after studying the mesa document)
+        system_prompt = """
+        You are a coding assistant specialized in social simulation models using MESA framework.
+        You task is to generate a list of shared instructions for multiple sub-coding agents.
+        These instructions should ensure compatibility and coherence across different code files.
+        Your output should be a json list of instructions, following this structure:
+        { "agents":[
+            {"class_name": ...,
+            "attributes": [...],
+            "methods": [...]}
+            ],
+        "model":[{
+            "class_name": ...,
+            "Scheduler": [...],
+            "grid_type": [...],
+            "imports": [...]}
+            ],
+        "parameters":{...},
+        "dependencies":{...}
+        }
+        An example insturction can be:
+        { "agents":[
+            {"class_name": SocialAgent,
+            "attributes": ["opinions", "neighbours"],
+            "methods": ["step()", "interact()"]}
+            ],
+        "model":[{
+            "class_name": SocialModel,
+            "Scheduler": ["RandomActivation"],
+            "grid_type": ["MultiGrid"],
+            "imports": ["SocialAgent from agent.py"]}
+            ],
+        "parameters":{"num_agents": 100, "width": 10, "height": 10},
+        "dependencies":{"model.py": ["agent.py", "environment.py"]}
+        }
+        """
+
+        user_prompt = f"""
+        Please generate a list of shared instructions for multiple sub-coding agents based on the following conceptual model: {conceptual_model}.
+        The instructions should ensure compatibility and coherence across different code files.
+        Please follow the structure and example provided in the system prompt.
+        """
+
+        # call the LLM
+        LLM_model = self.client.chat.completions.create(
+            model=self.model_name,
+            messages = [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt}
+            ],
+            temperature=0.2,
+        )
+
+        # parse the responses
+        try:
+            shared_instructions = json.loads(LLM_model.choices[0].message.content)
+        except json.JSONDecodeError:
+            print("Error decoding JSON from LLM response:")
+            match = re.search(r'\{.*\}', LLM_model.choices[0].message.content, re.DOTALL)
+            if match:
+                shared_instructions = json.loads(match.group(0))
+            else:
+                print("No valid JSON found in LLM response:")
+                raise ValueError("Invalid JSON response from LLM")
+            
+        return shared_instructions
+
+    def task_division(self,shared_instructions):
+        """
+        This function divides the overall coding task into sub-components.
+        It then assigns each sub-component to a specific sub-coding agent with the shared instructions
+        """
+        # load the instructions and conceptual model
+        # shared_instructions = json.dumps(shared_instructions, indent=2)
+        with open(self.conceptual_model, "r") as f:
+            conceptual_model = json.load(f)
+
+        code_snippets = {}
+        for module_name, sub_agent in self.sub_agents.items(): # assign task to each sub-coding agent
+            code = sub_agent.code_generator(shared_instructions, conceptual_model)
+            code = code.replace("```python\n", "").strip() # remove unnecessary markdown formatting if any
+            code = code.replace("```", "").strip()
+
+            print(f"Generated code for {module_name}")
+            code_snippets[module_name] = code # collect the generated code
+
+        return code_snippets
+
+    def code_integration_check(self, code_snippets):
+        """
+        This function collects all code snippets from sub-coding agents and do a testing run.
+        It reports issues and feedbacks to the relevant sub-coding agents for revision.
+        It outputs the final complete codebase if all components function correctly.
+        """
+        failed = []
+        for module_name, code in code_snippets.items():
+            try:
+                compile(code, module_name, 'exec')
+                print(f"{module_name} compiled successfully.")
+            except SyntaxError as e:
+                print(f"Syntax error in {module_name}: {e}")
+                failed.append(module_name)
+        
+        # save the code to a temporary directory
+        file_name = "generated_code"
+        os.makedirs(file_name, exist_ok=True)
+        for module_name, code in code_snippets.items():
+            with open(os.path.join(file_name, module_name), "w") as f:
+                f.write(code)
+        
+        return failed
+
+    def run_pipeline(self):
+        """
+        This function runs the entire code generation pipeline.
+        """
+        print("Step 1: Generating shared instructions based on the conceptual model...")
+        shared_instructions = self.shared_instructions_generator()
+        print(f"Shared Instructions: {shared_instructions}")
+
+        print("Step 2: Dividing tasks among sub-coding agents...")
+        code_snippets = self.task_division(shared_instructions)
+        print(f"Generated key files: {code_snippets.keys()}")
+
+        print("Step 3: Integrating code snippets and performing testing...")
+        failed_modules = self.code_integration_check(code_snippets)
+        if failed_modules:
+            print(f"Code generation pipeline completed with errors in: {failed_modules}")
+        else:
+            print("Code generation pipeline completed successfully.")
+
+#TODO: now each sub-coding agent is generating the same code, need to differentiate the role more clearly
+#TODO: need to implement a feedback loop for error correction
--- code generator/main.py
+++ code generator/main.py
@@ -1,8 +1,13 @@
 # main file to run the LLM module
-from coding_agent import CodingAgent
+from coding_agent import CodingAgent, MasterAgent, SubCodeAgents
 import os
 
 json_path = os.path.join ("..", "model construct assistant", "Conceptual Model.json")
-code = CodingAgent("gpt-4o-mini")
-generated_code = code.code_generation(json_path = json_path)
-print(generated_code)
+# code = CodingAgent("gpt-4o-mini")
+# generated_code = code.code_generation(json_path = json_path)
+# print(generated_code)
+
+# run the coding agent pipeline here
+master_agent = MasterAgent(json_path,"gpt-4o-mini")
+master_agent.run_pipeline()
+
